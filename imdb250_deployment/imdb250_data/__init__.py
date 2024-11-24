0d2eb601c0c8a2c6b35f58cc6b6408e4860ca4fb
Domingos de Eulária Dumba <110714056+domingosdeeulariadumba@users.noreply.github.com>
2024-11-24 14:55:42 +0100

    Add files via upload

---
+++ imdb250_deployment/imdb250_data/__init__.py
@@ -0,0 +1,2 @@
+# imdb250_data\__init__.py
+from .imdb250_data import Imdb250Data
---
+++ imdb250_deployment/imdb250_data/imdb250_data.py
@@ -0,0 +1,83 @@
+# Dependencies
+import joblib as jbl
+import redis
+import pandas as pd
+import json
+import os
+from requests.auth import HTTPBasicAuth
+from urllib.parse import urlparse
+
+
+
+# Taking the response of the serialized data
+imdb250_data_zipped_file = 'imdb250_data_zipped.joblib'
+imdb250_token = os.getenv('IMDB250_TOKEN')
+
+# Deserializing the data and passing it as list of value (dataframes) and key (file name)
+imdb250_data = jbl.load(imdb250_data_zipped_file)
+imdb250_data_items = list(imdb250_data)  
+
+# Establishing Redis connection using environment variables (with Render details)
+redis_url = 'redis://red-ct1cs63tq21c73enkkkg:6379'
+parsed_url = urlparse(redis_url)
+r = redis.StrictRedis(
+    host=parsed_url.hostname,
+    port=parsed_url.port,
+    password=None,
+    decode_responses=True,
+    socket_timeout=30
+)
+
+
+# Creating a class for retrieving data for deployment
+
+class Imdb250Data:
+    def compute_cached_data(self, data_items):
+        
+        value, key = data_items
+        
+        json_data = value.to_json()            
+        r.set(key, json_data)
+        cached_data = pd.DataFrame(json.loads(json_data))  
+        
+        return cached_data
+
+    
+    def get_cached_data(self, data_items):
+        
+        value, key = data_items
+        cached_data = r.get(key)
+        if cached_data:
+                return pd.DataFrame(json.loads(cached_data))
+        else:
+                return self.compute_cached_data(data_items)
+
+    # Method for fetching the IMDb 250 in a dataframe
+    def imdb250(self):
+        return self.get_cached_data(imdb250_data_items[0])
+    
+    
+    # Method for getting dataframe with similarity scores
+    def imdb_250_similarities(self):
+        return self.get_cached_data(imdb250_data_items[1])
+        
+               
+    # Method for getting recommendations based on user preference
+    def get_recommendations(self, user_preferences, 
+                            recommendations_data = imdb250_data_items[1][0]):
+        similarities_df = self.imdb_250_similarities()
+
+        cross_df = similarities_df.reindex(similarities_df.columns)
+
+        preferences_series = pd.concat([cross_df[preference]
+                                        for preference in user_preferences]
+                                       )
+        ordered_series = preferences_series.sort_values(
+            ascending=False).to_frame('score')
+        filtered_series = ordered_series[
+            ~(ordered_series.index.isin(user_preferences))]
+        top_recommendations = filtered_series.query('score  >= .55')
+        if len(top_recommendations) >= 5:
+            return list(top_recommendations.index[:5])
+        else:
+            return list(filtered_series.index[:5])
